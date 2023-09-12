381448c0a6ee2d46a90647f14b5cd0cef7d071ef
carrotjived <ona23001@byui.edu>
2023-09-12 08:12:51 +0800

    completed assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter Grade Percentage: ");
+        string gradePrompt = Console.ReadLine();
+        int grade = int.Parse(gradePrompt);
+
+        int remain = grade%10;
+
+        string finalGrade = "";
+
+        string sign = "";
+
+        string displayGrade = "";
+
+        if (grade>=90)
+        {
+            finalGrade = "A";
+        }
+        else if (grade>=80)
+        {
+            finalGrade = "B";
+        }
+        else if (grade>=70)
+        {
+            finalGrade = "C";
+        }
+        else if (grade>=60)
+        {
+            finalGrade = "D";
+        }
+        else if (grade<60)
+        {
+            finalGrade = "F";
+        }
+        else 
+        {
+            Console.WriteLine("Incorrect Grade!");
+        }
+    
+        if (remain>=7)
+        {
+            sign = "+";
+        }
+        else {
+            sign = "-";
+        }
+
+        displayGrade = finalGrade + sign;
+        
+        if (displayGrade == "A+")
+        {
+            displayGrade = "A";
+        }
+        else if (displayGrade == "F+" || displayGrade == "F-")
+        {
+            displayGrade = "F";
+        }
+        
+        Console.WriteLine($"Your grade is: {displayGrade}");
+
+
+        if (grade>=70)
+        {
+            Console.WriteLine("Congratulations you have passed!");
+        }
+        else {
+            Console.WriteLine("Life is not yet over! You can do better!");
+        }
+        
     }
 }
