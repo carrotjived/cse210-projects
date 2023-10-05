0cc10147f6813f3a799926f1153b590c37ddceec
carrotjived <ona23001@byui.edu>
2023-10-05 22:20:11 +0800

    finished journal

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,18 @@
+
+
+public class Entry {
+    public string _date;
+    public string _promptQuestion;
+    public string _entry;
+
+    public void DisplayEntry () {
+        Console.WriteLine($"{_date} - {_promptQuestion}  \n'{_entry}'\n");
+        
+    }
+    
+
+ 
+
+
+
+}
