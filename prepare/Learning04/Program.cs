b63b390a3b0dc2fc5c6e841604d63fd2756cf15c
carrotjived <ona23001@byui.edu>
2023-10-26 19:31:14 +0800

    inheritance learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment student = new("Sameul Bennet", "Multiplication");
+        Console.Clear();
+        Console.WriteLine(student.GetSummary());
+        MathAssignment mathStudent = new("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathStudent.GetSummary());
+        Console.WriteLine(mathStudent.GetHomeworkList());
+        WritingAssignment writeStudent = new("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writeStudent.GetSummary());
+        Console.WriteLine(writeStudent.GetWritingInformation());
+
     }
 }
